305f3aee7ec622111495a42453d39407e3220d9e
patscan <patscan@gmail.com>
2013-05-10 13:40:13 -0700

    changed game_moves to moves

--- db/migrate/20130510120311_creategames.rb
+++ db/migrate/20130510120311_creategames.rb
@@ -2,7 +2,7 @@ class Creategames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.integer :winner_id
-      t.string :game_moves
+      t.string :moves
     end
   end
 end
