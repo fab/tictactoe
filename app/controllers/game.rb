64b76fea20764bc6c5f1397c2f11058fcd3656d9
fmackojc <fab.mackojc@gmail.com>
2013-05-10 18:44:46 -0700

    added extra route to generate new games for testing, game now updates across two different browsers

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,10 +1,33 @@
 get '/game/:id' do
+  # game = Game.find(params[:id])
+  # game.update_attributes(moves: [])
+  @gameid = params[:id]
   erb :game
 end
 
+get '/creategame' do
+  game = Game.create(moves: '')
+  redirect "/game/#{game.id}"
+end
+
 post '/game/:id' do
+  puts params
   content_type :json
   game = Game.find(params[:id])
-  {GameID: game.id, Moves: game.moves, 
-  Player1: game.users[0].username, Player2: game.users[1].username, Winner: game.winnner_id}.to_json
+  puts game
+  new_moves = game.moves + params[:box]
+  game.update_attributes(moves: new_moves)
+  moves_array = new_moves.split('')
+  # {GameID: game.id, Moves: game.moves, 
+  # Player1: game.users[0].username, Player2: game.users[1].username, Winner: game.winnner_id}.to_json
+  {GameID: game.id, Moves: moves_array}.to_json
+end
+
+post '/checkmoves/:id' do
+  content_type :json
+  game = Game.find(params[:id])
+  moves_array = game.moves.split('')
+  # {GameID: game.id, Moves: game.moves, 
+  # Player1: game.users[0].username, Player2: game.users[1].username, Winner: game.winnner_id}.to_json
+  {GameID: game.id, Moves: moves_array}.to_json
 end
--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,92 @@
-var turn = 0;
-var winningCombos =
+var playerTurn = 0;
+var winningCombos = '';
+var board = [];
+var content = [];
 
-function renderBoard(){
-
+function initalBoardRender(){
+  for(var i = 0; i <= 8; i++) {
+    board[i] = false;
+    content[i] = "";
+  }
 }
 
-$(document).ready(function() {
+function turnBoardRender(game){
+  moves = game.Moves;
+  $.each(moves, function(index, space){
+    if (index % 2 === 0) {
+      board[space] = true;
+      content[space] = "X";
+    } else {
+      board[space] = true;
+      content[space] = "O";
+    }
+  });
+  redrawTable(content);
+}
 
-  
+function redrawTable(content){
+  $.each(content, function(index, xOrO){
+    $('#' + index).text(xOrO);
+  });
+}
+
+function ajax(choice) {
+  $.ajax({
+    url: '/game/' + gameID, //get game id
+    method: 'post',
+    data: { box : choice }
+  }).done(function(response){
+    turnBoardRender(response);
+  });
+}
+
+function checkMoves() {
+  $.ajax({
+    url: '/checkmoves/' + gameID, //get game id
+    method: 'post'
+  }).done(function(response){
+    turnBoardRender(response);
+  });
+}
+
+
+$(document).ready(function() {
+  // console.log(myclass.getUniqueWindowId( thisSession ));
+  initalBoardRender();
+  setInterval(checkMoves, 10);
+  gameID = $('#board').data('gameid');
+  console.log(gameID);
+  $('#board').on('click', 'td', function() {
+    console.log('clicked');
+    var id = $(this).attr('id');
+    console.log(id);
+    turn(id);
+  });
+
+  function turn(choice) {
+    console.log(board);
+    console.log(choice);
+    console.log(typeof choice);
+    choiceNum = parseInt(choice);
+    console.log(typeof choiceNum);
+    if(board[choice] === false) {
+      if(playerTurn % 2 === 0) {
+        content[choice] = "X";
+        board[choice] = true;
+        ajax(choice);
+      } else {
+        content[choice] = "O";
+        board[choice] = true;
+        ajax(choice);
+      }
+      playerTurn++;
+    } else {
+      alert("That space is filled, homes!");
+    }
+  }
 });
+
+  // take moves array from ruby
+  // make a moves array in JS
+  // after a successful choice push choice to moves array
+  // send moves array to ruby database
